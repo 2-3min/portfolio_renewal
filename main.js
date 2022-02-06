d7fcf51c0846112f6b07c7d77d571352e9004fd1
2-3min <foreverything1023@gmail.com>
2022-02-06 21:14:06 +0900

    Responsive web Design : Project List

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 //slide
+const project = document.querySelector(".project");
 const projectSlider = document.querySelector(".project__slider");
 const projectItems = document.querySelectorAll(".project__item");
 
@@ -9,18 +10,27 @@ const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
 let counter = 0;
-const widthSize = projectItems[0].clientWidth;
+let widthSize = projectItems[0].clientWidth;
 
-
+if(counter === 0) prevButton.style.display = "none";
 
 prevButton.addEventListener("click", () => {
   counter--;
   projectSlider.style.transform = `translateX(${-widthSize * counter}px)`;
   projectSlider.style.transition = 'transform 0.4s ease-in-out';
+  prevButton.style.display = counter === 0 ? 'none' : 'inline';
+  nextButton.style.display = counter === (projectItems.length - 1) ? 'none' : 'inline';
 });
+
 
 nextButton.addEventListener("click", () => {
   counter++;
   projectSlider.style.transform = `translateX(${-widthSize * counter}px)`;
   projectSlider.style.transition = 'transform 0.4s ease-in-out';
+  prevButton.style.display = counter === 0 ? 'none' : 'inline';
+  nextButton.style.display = counter === (projectItems.length - 1) ? 'none' : 'inline';
+});
+
+window.addEventListener("resize", () => {
+  widthSize = (window.innerWidth) < 1000 ? window.innerWidth : 1000; 
 });
